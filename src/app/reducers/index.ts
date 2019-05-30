7a8335b3c164ec5910687d204e4836eeea4504c9
gogirl10102 <50113159+gogirl10102@users.noreply.github.com>
2019-05-30 20:09:01 +0000

    Redux EOD 2

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,14 @@
+
+import * as fromCounter from './counter.reducer';
+
+export interface State {
+  counter: fromCounter.State;
+}
+
+export const reducers = {
+  counter: fromCounter.reducer
+};
+
+// Selector functions
+
+export const selectCurrentCount = (state: State) => state.counter.current;
